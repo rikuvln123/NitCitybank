e51203b6aba9fd507a147f73fdb0e86dd9bf8c64
DEV2 <Dev2@gmail.com>
2020-04-17 06:20:25 +0530

    changed by dev2

--- Demoproj1/src/com/nt/test/test.java
+++ Demoproj1/src/com/nt/test/test.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.nill.Utility;
+
 public class test {
 
+	public static void main(String[] args) {
+		Utility u =new Utility();
+		System.out.println(u.getsysDate());
+	}
 }
