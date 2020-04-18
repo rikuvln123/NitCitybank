63d0c4e26c7d1dd8d1204797a19ad3021a97642c
DEV2 <Dev2@gmail.com>
2020-04-18 05:37:52 +0530

    dev2

--- Demoproj1/src/com/nt/nill/Utility.java
+++ Demoproj1/src/com/nt/nill/Utility.java
@@ -8,11 +8,7 @@ public class Utility {
   }
   
   public void m1() {
-<<<<<<< HEAD
 	  System.out.println("Utility.m1( )...abc");
-=======
-	  System.out.println("Utility.m1() ....123");
->>>>>>> branch 'master' of https://github.com/rikuvln123/NitCitybank.git
   }
   
 }
